17a57f6f9330dde0cf00a8abea6e642d4d56f5a1
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-06-23 15:43:16 +0530

    Add files via upload

---
+++ Assignment 1/P1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P1 {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+            if (map.containsKey(remaining)) return new int[]{map.get(remaining), i};
+            map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = twoSum(nums, target);
+        System.out.println(Arrays.toString(res));
+
+    }
+}
