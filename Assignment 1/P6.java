6e48a17dbd7bdd7e4597b3c53d6f011f53823ea2
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-06-23 16:00:17 +0530

    Add files via upload

---
+++ Assignment 1/P6.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class P6 {
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        boolean result = containsDuplicate(nums);
+        System.out.println(result);
+    }
+}
