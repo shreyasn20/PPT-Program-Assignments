abfbac5f106f00cece651eeb379c734d6f6ce24e
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-06-23 15:50:04 +0530

    Add files via upload

---
+++ Assignment 1/P3.java
@@ -0,0 +1,24 @@
+class P3 {
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target) 
+                return mid;
+            else if (nums[mid] < target) 
+                low = mid + 1;
+            else
+                high = mid - 1;
+            
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        int result = searchInsert(nums,target);
+        System.out.println(result);
+    }
+}
