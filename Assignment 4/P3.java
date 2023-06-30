a58e600e78aa6a7f8085780aec9cdfd00b32eac7
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-06-30 23:17:00 +0530

    Create P3.java

---
+++ Assignment 4/P3.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class P3 {
+    public static int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] res = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] res = transpose(matrix);
+        for (int[] ele : res) {
+            System.out.println(Arrays.toString(ele));
+        }
+    }
+}
