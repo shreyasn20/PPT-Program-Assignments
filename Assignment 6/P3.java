8f4cb5f6946220d9d8c430f780372fae915945e0
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-07-03 23:32:39 +0530

    Create P3.java

---
+++ Assignment 6/P3.java
@@ -0,0 +1,22 @@
+class P3 {
+    public static boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return false;
+        int i = 0;
+        while (i + 1 < n && arr[i] < arr[i + 1])
+            i++;
+      
+        if (i == 0 || i == n - 1)
+            return false;
+
+        while (i + 1 < n && arr[i] > arr[i + 1])
+            i++;
+
+        return i == n - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1};
+        System.out.println(validMountainArray(arr));
+    }
+}
