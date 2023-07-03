7fd5e3f77491d7d4eb4b461320ed6d7854ecdefb
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-07-03 23:35:11 +0530

    Create P6.java

---
+++ Assignment 6/P6.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+class P6 {
+    public static int[] findOriginalArray(int[] nums) {
+        int n = nums.length;
+        if (n % 2 != 0) return new int[]{};
+      
+        int[] res = new int[n / 2];
+        int[] freq = new int[100001];
+
+        for (int ele : nums) freq[ele] += 1;
+
+        int index = 0;
+
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0 && i * 2 < 100001 && freq[i * 2] > 0) {
+                freq[i]--;
+                freq[i * 2]--;
+                if (freq[i * 2] < 0) return new int[]{};
+                res[index] = i;
+                index++;
+            }
+        }
+
+        for (int j : freq) if (j != 0) return new int[]{};
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 2, 6, 8};
+        System.out.println(Arrays.toString(nums));
+        int[] res = findOriginalArray(nums);
+        System.out.println(Arrays.toString(res));
+    }
+}
