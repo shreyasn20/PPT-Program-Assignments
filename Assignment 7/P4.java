0fd4b5eb075860745e1278eca267f16ac446f16f
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-07-03 23:42:50 +0530

    Create P4.java

---
+++ Assignment 7/P4.java
@@ -0,0 +1,19 @@
+class Problem4 {
+    public static String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ");
+        for (String word : words) {
+            StringBuilder temp = new StringBuilder(word);
+            temp.reverse();
+            sb.append(temp);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        String s1 = reverseWords(s);
+        System.out.println(s1);
+    }
+}
