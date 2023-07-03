a86322b6e32f4f0d7437f2b05548e154ac0e6b26
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-07-03 23:37:19 +0530

    Create P8.java

---
+++ Assignment 6/P8.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class P8 {
+    public static int[][] multiply(int[][] mat1, int[][] mat2) {
+        int r1 = mat1.length, c1 = mat1[0].length, c2 = mat2[0].length;
+        int[][] res = new int[r1][c2];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < r1; ++i) {
+            for (int j = 0; j < c1; ++j) {
+                if (mat1[i][j] != 0) {
+                    map.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+        for (int i = 0; i < r1; ++i) {
+            for (int j = 0; j < c2; ++j) {
+                if (map.containsKey(i)) {
+                    for (int k : map.get(i)) {
+                        res[i][j] += mat1[i][k] * mat2[k][j];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat1 = {{1, 0, 0}, {-1, 0, 3}};
+        int[][] mat2 = {{7, 0, 0}, {0, 0, 0}, {0, 0, 1}};
+        int[][] res = multiply(mat1, mat2);
+        for (int[] ele : res) {
+            System.out.println(Arrays.toString(ele));
+        }
+    }
+}
