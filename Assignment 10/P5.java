68260a5b4805990fa4fc0eb7ea060e432910b870
Shreyas Nimbhore <76162735+shreyasn20@users.noreply.github.com>
2023-07-23 12:04:41 +0530

    Create P5.java

---
+++ Assignment 10/P5.java
@@ -0,0 +1,20 @@
+class P5 {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
